cfbab4b357dd00becc32a943bdb5b522cd3b6efc
Kalpesh Patel <kalpeshpatel.it@gmail.com>
2021-01-08 12:10:49 -0600

    added environment file

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <h1>This is my react app</h1>
+      <p>BASE Url: {process.env.REACT_APP_BASE_URL}</p>
+      <p>API Key: {process.env.REACT_APP_API_KEY}</p>
     </div>
   );
 }
